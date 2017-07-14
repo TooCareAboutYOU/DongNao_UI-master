77a49f5bec1069ff97d32fcb46d16ee0316926fa
张帅 <1175133706@qq.com>
2017-07-14 15:31:58 +0800

    Activity live  cycle

--- unit1_uidrawprocess/src/main/java/com/dongnao/ui/master/drawprocess/MainActivity.java
+++ unit1_uidrawprocess/src/main/java/com/dongnao/ui/master/drawprocess/MainActivity.java
@@ -1,14 +1,61 @@
 package com.dongnao.ui.master.drawprocess;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "CustomTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "MainActivity---onCreate: ");
+        findViewById(R.id.btn_intent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestActivity.class));
+            }
+        });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "MainActivity---onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "MainActivity---onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "MainActivity---onDestroy: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "MainActivity---onPause: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "MainActivity---onResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "MainActivity---onRestart: ");
     }
 }
